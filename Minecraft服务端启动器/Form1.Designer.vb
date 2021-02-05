05e7b40756cc5284e988c227003bcbf11733ed14
luluxiaoyu <58876608+luluxiaoyu@users.noreply.github.com>
2021-02-05 08:29:38 +0800

    1.0

---
+++ Minecraft服务端启动器/Form1.Designer.vb
@@ -0,0 +1,258 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Process1 = New System.Diagnostics.Process()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("宋体", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 68)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "下载服务端"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("宋体", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button2.Location = New System.Drawing.Point(12, 97)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(132, 74)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "选择服务端"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("宋体", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button3.Location = New System.Drawing.Point(12, 187)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(83, 31)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "开服"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(150, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Minecraft Server"
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("宋体", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button4.Location = New System.Drawing.Point(199, 189)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(105, 29)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "强行关服"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.MenuText
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.Location = New System.Drawing.Point(312, 9)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(488, 306)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 322)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 12)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "服务端目录"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(79, 319)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(227, 21)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(394, 318)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(358, 21)
+        Me.TextBox3.TabIndex = 8
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"OP"})
+        Me.ComboBox1.Location = New System.Drawing.Point(312, 319)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(76, 20)
+        Me.ComboBox1.TabIndex = 9
+        Me.ComboBox1.Text = "快捷指令"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(150, 47)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(156, 124)
+        Me.ListBox1.TabIndex = 10
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Spigot", "CraftBukkit", "Paper", "Group Server", "Bedrock Server"})
+        Me.ComboBox2.Location = New System.Drawing.Point(150, 27)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(156, 20)
+        Me.ComboBox2.TabIndex = 11
+        Me.ComboBox2.Text = "Spigot"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 364)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(821, 22)
+        Me.StatusStrip1.TabIndex = 12
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(56, 17)
+        Me.ToolStripStatusLabel1.Text = "加载完成"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 221)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 16)
+        Me.Label3.TabIndex = 13
+        '
+        'Process1
+        '
+        Me.Process1.StartInfo.Domain = ""
+        Me.Process1.StartInfo.LoadUserProfile = False
+        Me.Process1.StartInfo.Password = Nothing
+        Me.Process1.StartInfo.StandardErrorEncoding = Nothing
+        Me.Process1.StartInfo.StandardOutputEncoding = Nothing
+        Me.Process1.StartInfo.UserName = ""
+        Me.Process1.SynchronizingObject = Me
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(758, 318)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(42, 23)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "发送"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("宋体", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button6.Location = New System.Drawing.Point(101, 188)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(92, 29)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "关服"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(821, 386)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Minecraft开服器框架"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Process1 As Process
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+End Class
---
+++ Minecraft服务端启动器/Form1.vb
@@ -0,0 +1,208 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Threading
+Public Class Form1
+    Private Delegate Sub AddMessageHandler(ByVal msg As String)
+    Dim server As New ProcessStartInfo("cmd.exe")
+    Private _process As Process = Nothing
+    Dim dwlurl, dwlfilename, serverurl
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("自己填进去服务端目录", 64, "自己填")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        server.WindowStyle = ProcessWindowStyle.Hidden
+        server.Arguments = "/c @ECHO OFF &&" & " java -jar " & serverurl & " nogui" & ComboBox1.Text
+        server.WorkingDirectory = Application.StartupPath & "/MinecraftServer"
+        server.UseShellExecute = False
+        server.RedirectStandardOutput = True
+        server.RedirectStandardInput = True
+        server.CreateNoWindow = True
+        _process = New Process()
+        _process.StartInfo = server
+        ' 定义接收消息的Handler
+        AddHandler _process.OutputDataReceived, New DataReceivedEventHandler(AddressOf Process1_OutputDataReceived)
+        _process.Start()
+        ' 开始接收
+        _process.BeginOutputReadLine()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            _process.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message, 16, "进程结束失败！（别点击那么多次）")
+        End Try
+    End Sub
+
+    Public Sub xzq1()
+        Dim hwq As HttpWebRequest
+        Dim hwp As HttpWebResponse
+        Dim colHeader As WebHeaderCollection  '响应头信息集合
+        Dim lngSize As Int64                  '要下载文件的总大小
+        Dim lngCurSize As Int64               '已经下载大小
+        Dim lngNet As Int64                   '计算网速用
+
+        Dim stRespones As Stream              '响应流
+        Dim st As FileStream                  '本地流
+        Dim intCurSize As Int64
+        Dim bytBuffer(512) As Byte           '缓存大小
+
+        Dim datLast As DateTime               '最后一次时间
+        Dim intDiff As Int32                  '两次时间差（秒）
+
+        datLast = Now   '取得开始时间
+        hwq = CType(HttpWebRequest.Create(dwlurl), HttpWebRequest) '请求对象创建
+        hwp = hwq.GetResponse        '取得响应对象
+        colHeader = hwp.Headers      '取得响应头
+        lngSize = colHeader.Get("Content-Length")  '取得要下载文件的大小
+
+        stRespones = hwp.GetResponseStream '取得响应流
+        st = New FileStream(Application.StartupPath & "\MinecraftServer\" & dwlfilename, FileMode.Create) '本地保存文件
+
+        intCurSize = stRespones.Read(bytBuffer, 0, bytBuffer.Length) '响应流中读取
+
+        Do While (intCurSize > 0) '只要有数据就继续
+            st.Write(bytBuffer, 0, intCurSize)     '写入本地文件
+            intDiff = DateDiff(DateInterval.Second, datLast, Now)
+
+            lngCurSize = lngCurSize + intCurSize
+            lngNet = lngNet + intCurSize              '单位时间内的下载量
+            If intDiff >= 1 Then
+                Label3.Text = "文件大小：" & Math.Round(lngSize / 1024 / 1024, 2) & " MB" & vbCrLf &
+                                "已经下载：" & Math.Round(lngCurSize / 1024 / 1024, 2) & " MB" & vbCrLf &
+                                "下载速度：" & CInt(lngNet / intDiff / 1024 / 1024) & " MB/s" & vbCrLf &
+                                "下载百分比：" & Math.Round(lngCurSize / lngSize * 100, 2) & " %"
+                datLast = Now
+                lngNet = 0
+            End If
+
+            Application.DoEvents()
+            intCurSize = stRespones.Read(bytBuffer, 0, bytBuffer.Length) '继续读取
+        Loop
+        Label3.Text = "下载完成！"
+        ToolStripStatusLabel1.Text = "下载完成！"
+        TextBox2.Text = Application.StartupPath & "\MinecraftServer\" & dwlfilename
+
+        st.Close()
+        stRespones.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckForIllegalCrossThreadCalls = False
+
+        '检测服务端目录
+        If System.IO.Directory.Exists(Application.StartupPath & "/MinecraftServer") = False Then
+            System.IO.Directory.CreateDirectory(Application.StartupPath & "/MinecraftServer")
+        End If
+    End Sub
+
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        If (ComboBox2.Text = "Spigot") Then
+            ListBox1.Items.Clear()
+            ListBox1.Items.Add("双击下载")
+            ListBox1.Items.Add("Spigot-1.16")
+            'ListBox1.Items.Add("Spigot-1.15")
+            'ListBox1.Items.Add("Spigot-1.14")
+            'ListBox1.Items.Add("Spigot-1.13")
+            'ListBox1.Items.Add("Spigot-1.12")
+            'ListBox1.Items.Add("Spigot-1.11")
+            ' ListBox1.Items.Add("Spigot-1.11")
+            'ListBox1.Items.Add("Spigot-1.10")
+            'ListBox1.Items.Add("Spigot-1.9")
+            'ListBox1.Items.Add("Spigot-1.8")
+        End If
+    End Sub
+    Private Sub ListBox1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles ListBox1.MouseDoubleClick
+        If ListBox1.SelectedItem.ToString = "Spigot-1.16" Then
+            dwlurl = "https://drive.dnxshare.xyz/2:/Spigot/spigot-1.16.5.jar"
+            dwlfilename = "Spigot-1.16.jar"
+            Label3.Text = "正在向下载源请求回源···"
+            Button1.Enabled = False
+            Dim xzq As Thread
+            xzq = New Thread(AddressOf xzq1)
+            xzq.Start()
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        serverurl = TextBox2.Text
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Try
+            _process.StandardInput.WriteLine(TextBox3.Text)
+            TextBox3.Text = ""
+        Catch ex As Exception
+            MsgBox(ex.Message, 16, "指令发送错误")
+        End Try
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        _process.StandardInput.WriteLine("stop")
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If (ComboBox1.Text = "OP") Then
+            TextBox3.Text = "op "
+        End If
+    End Sub
+
+    Private Sub Process1_OutputDataReceived(sender As Object, e As DataReceivedEventArgs) Handles Process1.OutputDataReceived
+        Dim handler As AddMessageHandler = Function(msg As String)
+                                               '判断日志类型
+                                               If InStr(1, msg, "INFO") <> 0 Then
+                                                   TextBox1.AppendText("[信息]" & msg + Environment.NewLine)
+                                               Else
+                                                   If InStr(1, msg, "WARN") <> 0 Then
+                                                       TextBox1.AppendText("[警告]" & msg + Environment.NewLine)
+                                                   Else
+                                                       If InStr(1, msg, "ERROR") <> 0 Then
+                                                           TextBox1.AppendText("[错误]" & msg + Environment.NewLine)
+                                                       Else
+                                                           TextBox1.AppendText("[未知]" & msg + Environment.NewLine)
+                                                       End If
+                                                   End If
+                                               End If
+                                               '检测是否关闭
+                                               If InStr(1, msg, "All chunks are saved") <> 0 Then
+                                                   ToolStripStatusLabel1.Text = "服务端关闭！"
+                                               End If
+                                           End Function
+        If Me.TextBox1.InvokeRequired Then
+            Me.TextBox1.Invoke(handler, e.Data)
+        End If
+    End Sub
+
+    Private Sub TextBox3_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox3.KeyDown
+        If e.KeyCode = 13 Then
+            Try
+                TextBox3.Text = ""
+                _process.StandardInput.WriteLine(TextBox3.Text)
+            Catch ex As Exception
+                MsgBox(ex.Message, 16, "指令发送错误")
+            End Try
+        End If
+    End Sub
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        _process.StandardInput.WriteLine("stop")
+    End Sub
+End Class
